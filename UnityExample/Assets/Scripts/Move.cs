17e1c2b929af8d422c33897e4f62aea6a59106ac
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-09 09:24:59 +0100

    v0.1 Speedo busta

--- UnityExample/Assets/Scripts/Move.cs
+++ UnityExample/Assets/Scripts/Move.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed = 0.3f;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        trasnform.Translate();
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Jump") * Time.deltaTime,
+            Input.GetAxis("Vertical") * Time.deltaTime);
 	}
 }
