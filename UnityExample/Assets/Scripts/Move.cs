e4919593bdf4ac5edfcdf57d74c3c381c91b1b69
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-09 10:11:35 +0100

    v.0.2 physics

--- UnityExample/Assets/Scripts/Move.cs
+++ UnityExample/Assets/Scripts/Move.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 
-    public float speed = 0.3f;
+    //public float speed = 0.3f;
+    public float forceValue = 5f;
+    Rigidbody rigidbody;
 	void Start () {
+        // Cogemos la referencia para que sea más óptimo.
+        rigidbody = GetComponent<Rigidbody>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Jump") * Time.deltaTime,
-            Input.GetAxis("Vertical") * Time.deltaTime);
+        /*transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Jump") * Time.deltaTime,
+            Input.GetAxis("Vertical") * Time.deltaTime);*/
 	}
+
+    /// <summary>
+    /// Metodo usado para los movimientos fisicos. Por defecto son 50 veces por segundo.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"),Input.GetAxis("Vertical")) * forceValue);
+    }
 }
